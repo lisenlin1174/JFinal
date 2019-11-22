2052aa750188ac1affb1a43af1bbf1ec97c59d46
lingege45 <2779395322@qq.com>
2019-11-22 11:33:55 +0800

    ...

--- src/main/java/com/jfinal/config/JfinalConfig.java
+++ src/main/java/com/jfinal/config/JfinalConfig.java
@@ -2,6 +2,10 @@ package com.jfinal.config;
 
 import com.jfinal.controller.IndexController;
 import com.jfinal.controller.PhotoController;
+import com.jfinal.ext.interceptor.SessionInViewInterceptor;
+import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
+import com.jfinal.plugin.activerecord.dialect.MysqlDialect;
+import com.jfinal.plugin.druid.DruidPlugin;
 import com.jfinal.template.Engine;
 
 public class JfinalConfig extends JFinalConfig{
@@ -19,11 +23,27 @@ public class JfinalConfig extends JFinalConfig{
 
     }
 
-    public void configPlugin(Plugins plugins) {
+    public void configPlugin(Plugins plugins) {                              //数据库配置
+        loadPropertyFile("myconfig.properties");     //加载配置文件 文件放在/src/main/resources下
+        DruidPlugin druidPlugin=new DruidPlugin(              //添加Druid数据库连接池插件，分别传入Druid可以根据jdbc url自动识别驱动类型
+                getProperty("jdbc.url"),
+                getProperty("jdbc.username","root"),   //数据库连接用户名
+                getProperty("jdbc.password","root")    //数据库连接密码
+        );
+        plugins.add(druidPlugin);                             //通过add方法添加插件
+
+        //添加Activeecord插件
+        ActiveRecordPlugin activeRecordPlugin=new ActiveRecordPlugin(druidPlugin);
+        plugins.add(activeRecordPlugin);
+        activeRecordPlugin.setDialect(new MysqlDialect());   //设置Activeecord插件数据库方言，默认为Mysqlialect
+        activeRecordPlugin.setDevMode(getPropertyToBoolean("devMode",false));  //根据配置文件配置Activeecord插件的开发模式
+        activeRecordPlugin.setShowSql(getPropertyToBoolean("showSql",false));  //根据配置文件配置Activeecord插件的开发是否输出SQL语句
 
     }
 
     public void configInterceptor(Interceptors interceptors) {
+        //配置session拦截器 可以在模板中直接调用session
+        interceptors.add(new SessionInViewInterceptor());
 
     }
 
